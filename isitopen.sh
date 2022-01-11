90506bb693c02298211f8e76865b571067985203
Cl0udk1ck3r <96272542+Cl0udk1ck3r@users.noreply.github.com>
2022-01-11 01:20:38 +0000

    It's now a bash alias!
    Added some more functionality that makes it useful enough to justify using as a bash alias.

--- isitopen.sh
+++ isitopen.sh
@@ -5,11 +5,24 @@ echo "What program do you seek?"
 read programname
 
 #Stores the variable to check for emptiness
-answer="$(ps -e | grep $programname | grep -v isdiscordopen)"
+answer="$(ps -e | grep $programname | grep -v "isitopen")"
 
 if [[ -n $answer ]]
 then
-	printf "It's open. "
+	echo "It's open. "
 else
-	printf "Nope. "
+	echo "Nope. "
+	exit
+fi
+
+echo "Do you want me to fix that for you?"
+#Prompt for binary input
+read answer
+
+if [[ $answer == yes ]]
+then
+	sudo pkill -f $programname
+	echo "It is done."
+else
+	echo "Understood. My blade is ready."
 fi
